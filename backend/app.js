857bcbf25720232c5797d8fcda62d6063b4ae3cf
bhupendra700 <bhupendra7021@gmail.com>
2025-09-15 20:34:26 +0530

    all done

--- backend/app.js
+++ backend/app.js
@@ -13,7 +13,7 @@ const axios = require("axios")
 const FormData = require("form-data")
 
 
-const origin = ["http://localhost:5173"];
+const origin = ["https://pixorafree.netlify.app"];
 app.use(cors({ origin }))
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
